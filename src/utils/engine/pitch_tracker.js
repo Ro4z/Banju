9d137f50c60433190f9a7969c1f1edd505a44c71
FYLSunghwan <sunghwan519@hotmail.com>
2020-08-10 22:38:06 +0900

    add: Add pitch tracker module

---
+++ src/utils/engine/pitch_tracker.js
@@ -0,0 +1,18 @@
+import {NativeModules, NativeEventEmitter} from 'react-native';
+
+class PitchTracker extends NativeEventEmitter {
+    constructor(nativeModule) {
+        super(nativeModule);
+
+        this.printModel = nativeModule.printModel
+        this.start = nativeModule.start
+        this.stop = nativeModule.stop
+        this.monitor = function() {
+            while(1) {
+                console.log(this.printModel())
+            }
+        }
+    }
+}
+
+export default new PitchTracker(NativeModules.PitchTracker)
