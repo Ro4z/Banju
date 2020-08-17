4e1859ce3b9328d88a7e2c376e1001643bfed0bc
You In Seong <insung1217@gmail.com>
2020-08-17 17:53:10 +0900

    feat: Add tutorial component

--- src/screens/Practice.js
+++ src/screens/Practice.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import Modal from 'react-native-modal';
+
 import Game from '../components/game';
+import Tutorial from '../components/game/Tutorial';
 
 const Practice = () => {
+  const [openModal, setOpenModal] = useState(false);
+
+  useEffect(() => {
+    setOpenModal(true);
+  }, []);
+
+  const toggleModal = () => {
+    setOpenModal(!openModal);
+  };
+
   return (
     <View style={styles.mainContainer}>
       <Game />
+      <Modal
+        isVisible={openModal}
+        animationIn="zoomIn"
+        animationOut="fadeOut"
+        style={styles.modal}>
+        <Tutorial onPress={toggleModal.bind()} />
+      </Modal>
     </View>
   );
 };
@@ -16,5 +36,10 @@ const styles = StyleSheet.create({
   mainContainer: {
     width: '100%',
     height: '100%',
+  },
+  modal: {
+    margin: 0, // This is the important style you need to set
+    alignItems: undefined,
+    justifyContent: undefined,
   },
 });
