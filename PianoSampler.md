21e2301bf6c3755a12eeb290029535dcbb769802
FYLSunghwan <sunghwan519@hotmail.com>
2020-08-20 20:40:17 +0900

    feat: Add PianoSampler module for piano sound

---
+++ PianoSampler.md
@@ -0,0 +1,14 @@
+# PitchTracker React Native
+
+## How to Import
+
+```javascript
+import PianoSampler from '{path}/utils/engine/piano_sampler'
+```
+
+### Functions
+
+```javascript
+PianoSampler.playNote(midiNum: Integer, velocity: Integer) // Velocity : 0~127
+pitchTracker.stop(midiNum: Integer)
+```
---
+++ ios/PianoSampler.swift
@@ -0,0 +1,43 @@
+//
+//  PianoSampler.swift
+//  Banju
+//
+//  Created by 김성환 on 2020/08/18.
+//
+
+import Foundation
+import UIKit
+import AudioKit
+
+@objc(PianoSampler)
+class PianoSampler: NSObject {
+  
+  let conductor = Conductor.sharedInstance
+  
+  @objc(playNote:velocity:)
+  func playNote(_ midiNum: NSInteger, velocity: NSInteger) {
+    DispatchQueue.main.async {
+      let midiNum_ = MIDINoteNumber(midiNum)
+      let velocity_ = MIDIVelocity(velocity)
+      self.conductor.playNote(note: midiNum_, velocity: velocity_)
+    }
+  }
+  
+  @objc(stopNote:)
+  func stopNote(_ midiNum: NSInteger) {
+    let midiNum_ = MIDINoteNumber(midiNum)
+    conductor.stopNote(note: midiNum_)
+  }
+  
+  @objc
+  func prepare() {
+    conductor.allNotesOff()
+    conductor.useSound("up")
+    print("prepared")
+  }
+  
+
+  static func requiresMainQueueSetup() -> Bool {
+    return true
+  }
+}
