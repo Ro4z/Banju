4eb450cc49cad6e0aad97a40f60e4a98d26ac89e
You In Seong <insung1217@gmail.com>
2020-08-24 22:16:07 +0900

    feat: Add Note component using in Tutorial

---
+++ src/components/game/Note.js
@@ -0,0 +1,43 @@
+import React, {PureComponent} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {RADIUS} from '../../constants/game/note';
+import {HEIGHT} from '../../constants/dimensions';
+
+const PinkNote = ({position}) => {
+  const x = position[0] - RADIUS / 2;
+  const y = position[1] - RADIUS;
+  return (
+    <View
+      style={[
+        styles.finger,
+        {left: x, top: y, backgroundColor: 'rgb(150,190,230)'},
+      ]}
+    />
+  );
+};
+
+const BlueNote = ({position, color}) => {
+  const x = position[0] - RADIUS / 2;
+  const y = position[1] - RADIUS;
+  return (
+    <View
+      style={[
+        styles.finger,
+        {left: x, top: y, backgroundColor: 'rgb(170,230,110)'},
+      ]}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  finger: {
+    borderColor: '#CCC',
+    borderWidth: 4,
+    borderRadius: 5,
+    width: 25,
+    height: 100,
+    position: 'absolute',
+  },
+});
+
+export {PinkNote, BlueNote};
