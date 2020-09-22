2fc2b77f4366200bd60fe2da74288006d3ea6881
You In Seong <insung1217@gmail.com>
2020-09-22 13:21:07 +0900

    build: Add alias, typescript setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,24 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+   plugins: ['import'],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        alias: {
+          @assets: './src/assets',
+          @components: './src/components',
+          @atoms: './src/components/atoms',
+          @molecules: './src/components/molecules',
+          @organisms: './src/components/organisms',
+          @navigations: './src/navigations',
+          @scenes: './src/scenes',
+          @services: './src/services',
+          @styles: './src/styles',
+          @utils: './src/utils',
+        },
+      },
+    },
+  },
 };
