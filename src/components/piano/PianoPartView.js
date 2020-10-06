45862c36f2dbaee4481e80be524b9af9b91db892
You In Seong <insung1217@gmail.com>
2020-10-07 03:32:22 +0900

    feat: Apply piano-sampler to PianoPartView

--- src/components/piano/PianoPartView.js
+++ src/components/piano/PianoPartView.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
-import Player from 'react-native-sound-player';
+import PianoSampler from 'react-native-piano-sampler';
 
 import Piano from './Piano';
-import PianoSampler from '../../utils/engine/piano_sampler';
 import {BACKGROUND_COLOR} from '../../constants/color';
+
+PianoSampler.prepare();
 
 const PianoPartView = ({position, firstKey = 'c4', lastKey = 'b4'}) => {
   // const x = position[0];
@@ -20,7 +21,6 @@ const PianoPartView = ({position, firstKey = 'c4', lastKey = 'b4'}) => {
         onStopNoteInput={(chord, midi) => {
           PianoSampler.stopNote(midi);
         }}
-        heightValue={150}
       />
     </View>
   );
