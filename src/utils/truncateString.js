f697a3a86c45ec9af3f7695e36db2a6d6404e2be
You In Seong <insung1217@gmail.com>
2020-10-12 01:24:57 +0900

    fix: Add comment and default value to function

--- src/utils/truncateString.js
+++ src/utils/truncateString.js
@@ -1,4 +1,9 @@
-const truncateString = (str, length) => {
+/**
+ * return String truncation result
+ * @param {String} str target string
+ * @param {Number} length truncation length
+ */
+const truncateString = (str, length = 10) => {
   if (str.length < length) return;
 
   return str.slice(0, length) + '...';
