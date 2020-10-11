9a0d42c43fe0831d3235f5d5905a1f3114a2c759
You In Seong <insung1217@gmail.com>
2020-10-12 02:03:37 +0900

    fix: Fix a typo

--- src/utils/truncateString.js
+++ src/utils/truncateString.js
@@ -4,7 +4,7 @@
  * @param {Number} length truncation length
  */
 const truncateString = (str, length = 10) => {
-  if (str.length < length) return;
+  if (str.length < length) return str;
 
   return str.slice(0, length) + '...';
 };
