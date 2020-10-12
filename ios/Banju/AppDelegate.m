d8f4f610ccbb62436536a9d769069059290d8767
You In Seong <insung1217@gmail.com>
2020-10-13 01:30:38 +0900

    build: Add kakao-login iOS config

--- ios/Banju/AppDelegate.m
+++ ios/Banju/AppDelegate.m
@@ -6,6 +6,7 @@
 #import "RNSplashScreen.h"
 #import "Orientation.h"
 #import <RNHomeIndicator.h>
+#import <KakaoOpenSDK/KakaoOpenSDK.h>
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -26,6 +27,31 @@ static void InitializeFlipper(UIApplication *application) {
 #endif
 
 @implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+                                      sourceApplication:(NSString *)sourceApplication
+                                              annotation:(id)annotation {
+    if ([KOSession isKakaoAccountLoginCallback:url]) {
+        return [KOSession handleOpenURL:url];
+    }
+
+    return false;
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+                                                options:(NSDictionary<NSString *,id> *)options {
+    if ([KOSession isKakaoAccountLoginCallback:url]) {
+        return [KOSession handleOpenURL:url];
+    }
+
+    return false;
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    [KOSession handleDidBecomeActive];
+}
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -63,3 +89,4 @@ static void InitializeFlipper(UIApplication *application) {
 }
   
 @end
+
