f9880d0237eb8b312e3fdfe64c04c04b91318441
You In Seong <insung1217@gmail.com>
2020-10-28 04:58:29 +0900

    feat: Add POST_USER endpoint

--- base.js
+++ base.js
@@ -3,11 +3,13 @@ const endpoint = 'http://api.dailybanju.com';
 const musicreg = '/musicreg';
 const playmeta = '/playmeta';
 const search = '/search';
+const user = '/user';
 
 const Base = {
   POST_MUSICREG: endpoint + musicreg,
   GET_PLAYMETA: `${endpoint + playmeta}/`,
   GET_SEARCH: `${endpoint + search}/`,
+  POST_USER: endpoint + user,
 };
 
 export default Base;
