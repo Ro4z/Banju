53af8830015f1a0b33713c87cf2083fe36f5d37f
You In Seong <insung1217@gmail.com>
2020-10-28 04:42:07 +0900

    feat: Add social login utils

--- src/utils/login/googleLogin.js
+++ src/utils/login/googleLogin.js
@@ -1,18 +1,23 @@
-import {GoogleSignin} from '@react-native-community/google-signin';
+import { GoogleSignin } from '@react-native-community/google-signin';
 
 GoogleSignin.configure({
-  webClientId:
-    '1070023131367-c3qkivj9heir03lou7ffd0n5fj0frmed.apps.googleusercontent.com',
+  webClientId: '1070023131367-c3qkivj9heir03lou7ffd0n5fj0frmed.apps.googleusercontent.com',
   offlineAccess: false,
 });
-const loginGoogle = async () => {
+const fetchGoogleLogin = async () => {
   GoogleSignin.signIn()
-    .then((res) => {
-      console.log(res);
+    .then(() => {
+      GoogleSignin.getTokens()
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     })
     .catch((err) => {
       console.log(err);
     });
 };
 
-export {loginGoogle};
+export { fetchGoogleLogin };
