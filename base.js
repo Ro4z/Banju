cf27ed1b81f1283b6932b5a4ec71a827d223299e
You In Seong <insung1217@gmail.com>
2020-10-30 21:58:10 +0900

    feat: Update endpoint in base.js

--- base.js
+++ base.js
@@ -4,12 +4,14 @@ const musicreg = '/musicreg';
 const playmeta = '/playmeta';
 const search = '/search';
 const user = '/user';
+const popular = '/popular';
 
 const Base = {
   POST_MUSICREG: endpoint + musicreg,
+  POST_USER: endpoint + user,
   GET_PLAYMETA: `${endpoint + playmeta}/`,
   GET_SEARCH: `${endpoint + search}/`,
-  POST_USER: endpoint + user,
+  GET_POPULAR: endpoint + popular,
 };
 
 export default Base;
